fede3579adc2ca8d941a69c80f8c5eba9cc47ba3
Denver D'Souza <denver@Denvers-Mac-mini.local>
2024-12-22 19:27:55 -0500

    Added Buttons and search Bar UI

--- server.js
+++ server.js
@@ -1,16 +1,72 @@
 const express = require('express');
 // const axios = require('axios');
 // const cors = require('cors');
-const dotenv = require('dotenv');
 const path = require('path');
+const dotenv = require('dotenv');
+const CryptoJS = require('crypto-js');
+const fetch = require('node-fetch');
 
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 3000;
+
+const authKey = process.env.AUTH_KEY;
+const secretKey = process.env.SECRET_KEY;
+const userAgent = process.env.USER_AGENT;
+const apiEndpoint = process.env.API_ENDPOINT;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-const PORT = process.env.PORT || 3000;
+// Shared authentication function
+function generateAuthHeaders() {
+    const apiHeaderTime = Math.floor(new Date().getTime() / 1000);
+    const hash = CryptoJS.SHA1(authKey + secretKey + apiHeaderTime).toString(CryptoJS.enc.Hex);
+
+    return {
+        'User-Agent': userAgent,
+        'X-Auth-Key': authKey,
+        'X-Auth-Date': apiHeaderTime.toString(),
+        'Authorization': hash
+    }
+}
+
+// Search for podcasts
+
+app.get('/api/search', async (req, res) => {
+    const query = req.query.q;
+    if (!query) {
+        return res.status(400).json({ error: 'Query Parameter is required!' });
+    }
+
+    const headers = generateAuthHeaders();
+
+    try {
+        const response = await fetch(`${apiEndpoint}/search/byterm?q=${encodeURIComponent(query)}`, {
+            method: 'GET',
+            headers: headers
+        });
+
+        if (response.ok && response.headers.get('content-type').includes('application/JSON')) {
+            const data = await response.json();
+            res.json(data);
+        } else {
+            const rawText = await response.text();
+            console.log('Raw Response: ', rawText);
+            res.status(500).json({ error: 'Invalid response from API', rawText });
+        }
+
+    } catch(error) {
+        console.error('Error fetching API: ', error.message);
+        res.status(500).json({ error: error.message });
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT} pointing to ${apiEndpoint}`);
+});
+
+
 // const PANTRY_ID = process.env.PANTRY_ID; 
 
 // Use your Pantry ID
